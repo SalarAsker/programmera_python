bddf4b0875c100e18cf4a0e3488a95345717774e
Salar Asker Zada <salar.askerzada@uddevalla.se>
2022-10-09 22:13:20 +0200

    Clr

---
+++ Modul2/2.5 Övningar/4.py
@@ -0,0 +1,10 @@
+# Skapa två variabler som följande, 
+# a = 10;
+# b = 20;
+# Koda en print som genererar följande utskrift
+# Summan är: 30
+
+a = 10
+b = 20;
+summan = a + b;
+print('Summa är:', summan)
---
+++ Modul2/2.5 Övningar/5.py
@@ -0,0 +1,13 @@
+# Nu skriver vi en ny version av program 4. Istället för att 
+# skapa variabler a och b med talen 10 och 20 ska vi läsa 
+# in data i båda variabler via input() metoden. Vi ska sedan 
+# addera båda variabler och försöka  skriva ut summan. 
+# Så här ser programkoden ut: (anta att vi ger 10 och 20 till programmet.)
+
+a = input('Ange nummer1:')
+b = input('Ange nummer2:')
+summan = a + b
+print('Summan är:', summan)
+
+# Om man variablerna får värden 10 och 20 blir resultatet 1020. 
+# input() metod returnerar stränger så vi adderar två stränger. 10
