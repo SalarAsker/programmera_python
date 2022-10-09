52d842f0a004cb711dbc8c520b9523dd68028da0
Salar Asker Zada <salar.askerzada@uddevalla.se>
2022-10-09 22:25:49 +0200

    Clr

---
+++ Modul2/2.8 Övningar/1.py
@@ -0,0 +1,7 @@
+# Läsa in ett heltal och räkna ut kvadraten på talet. Så här kan det se ut när man kör programmet. 
+# Skriv ett tal:12
+# Talet i kvadrat är: 144
+
+tal = int(input('Ange ett heltal: '))
+kvadrat = tal*tal
+print('Talet i kvadrat är: ', kvadrat)
---
+++ Modul2/2.8 Övningar/2.py
@@ -0,0 +1,8 @@
+# Skriv en ny version av programmet som räknar ut 
+# kvadraten till ett tal. I den nya versionen ska man kunna 
+# beräkna kvadraten av vilket tal som helst, int bara heltal (använd float()). 
+# Kör programmet och låt den först räkna ur kvadraten på 1,5 och sedan 1,6.
+
+tal = float(input('Ange ett heltal: '))
+kvadrat = tal*tal
+print('Talet i kvadrat är: ', kvadrat)
