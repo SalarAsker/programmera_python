422327bda2b23abaf1d67d61d5b63bb342f82ed8
Salar Asker Zada <salar.askerzada@uddevalla.se>
2022-10-09 19:32:36 +0200

    Clr

---
+++ Modul2/2.5 Övningar/1.py
@@ -0,0 +1,3 @@
+# Koda en print() metod och ge den flera argument att skriva ut.
+
+print( 'Vi har börjat med att', 'programmera i', 'Python', '.Programmering är kul!')
---
+++ Modul2/2.5 Övningar/2.py
@@ -0,0 +1,8 @@
+# Skriv en ny version av programmet ovan. Skapa två variabler, 
+# msg1 och msg2. Ge msg1 texten 'Vi börjat med att', 
+# msg2 texten 'programmera i'. Låt texten ‘Python’ sitta kvar. 
+# Koda nu en print() som skriver ut samma utskrift som i fråga 1.  
+
+msg1 = 'Vi har börjat med att'
+msg2 = 'programmera i'
+print(msg1, msg2, 'Python')
