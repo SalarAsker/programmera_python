7aef5e16571208fa8865dcca41fd3574f9f83856
Salar Asker Zada <salar.askerzada@uddevalla.se>
2022-10-16 00:19:14 +0200

    clr

---
+++ Module 3/3.3 Övningar/lösning.py
@@ -0,0 +1,11 @@
+# 1. Om temperatur är större än eller lika med 20 OCH om temperatur är mindre än eller lika med 30.
+# svar:  temp >= 20 and temp <= 30
+
+# 2. Om lönen är lika med 40000 ELLER om lönen är större än 40000.
+# svar: lön == 40000 or lön > 40000
+
+# 3. Om namn är lika med   'Johan' ELLER om namn är lika med  'JOHAN'
+# svar: namn == 'Johan' or namn == 'JOHAN'
+
+# 4. Om timme är lika med  12 OCH om minuter är lika med 15
+# svar: timme == 12 and minuter == 15
---
+++ Module 3/3.5 Övningar/1.py
@@ -0,0 +1,8 @@
+# Skriv ett program som läser in ett tal. 
+# Programmet ska sedan avgöra om talet är positivt 
+# eller negativt. En körning på programmet kan se ut som, 
+
+tal = int(input('Ange ett tal ?'))
+if tal < 0:
+    print('Talet är negativt!')
+print('Talet du angav', tal)
